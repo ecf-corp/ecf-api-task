4d75d699597ba6777557762b52161b3205dbba3d
ecf <ecf@ecfui-MacBookPro.local>
2024-10-11 09:50:08 +0900

    feat: 회원 관련 파일구조 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './product/product.module';
+import { CustomersModule } from './customers/customers.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     PrismaModule,
-    ProductsModule
+    ProductsModule,
+    CustomersModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
